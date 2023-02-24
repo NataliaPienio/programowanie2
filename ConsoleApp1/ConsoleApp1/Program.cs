57ea303ee7ba69626061c535e4682e6f224b9de9
NataliaPienio <90185181+NataliaPienio@users.noreply.github.com>
2023-02-24 10:29:04 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvFilePath = @"C:\Users\Unknow\Desktop\ConsoleApp1\csv\populacja.csv";
+
+            CsvReader csvReader = new CsvReader(csvFilePath);
+            City[] citiesAll = csvReader.ReadFirstNCities(3);
+
+            /*
+            foreach (var city in cities)
+            {
+                Console.WriteLine("Miasto");
+            }*/
+            StreamReader sr = new StreamReader(csvFilePath);
+            string line = sr.ReadLine();
+            City c = csvReader.ReadCityFromCsvLine(line);
+            Console.WriteLine(c.CityName);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/classes/CsvReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class CsvReader
+    {
+        private string csvFilePath = "";
+
+        public CsvReader(string _csvFilePath) {
+            csvFilePath= _csvFilePath;
+        }
+
+        public City[] ReadFirstNCities(uint nCities) 
+        {
+            //return null;
+            City[] cities = new City[nCities];
+            return cities;
+        }
+        public City ReadCityFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(', ');
+            string CityName = parts[0];
+            return new City(CityName);
+        }
+    }
+}
